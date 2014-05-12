aa2696118da341977e752a7308ee5424363bd935
CesarConopoima <cesar.conopoima@gmail.com>
2014-05-12 18:23:07 -0430

    second

--- Hi.vb
+++ Hi.vb
@@ -1,5 +1,5 @@
 Module Hello
   Sub Main()
-        MsgBox("Hello, World! is Cesar here") ' Display message on computer screen.
-  End Sub 
+        MsgBox("Hello, World! is Cesar YES") ' Display message on computer screen.
+    End Sub
 End Module
